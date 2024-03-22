951b12b854bd3da46f7bc941cab423f746a9a9c9
Jedaias Rodrigues <jedaiasrc7@hotmail.com>
2024-03-22 11:39:17 -0300

    Traefik configuration.

--- src/Presentation/OrchestrationLab.Api/Controllers/WeatherForecastController.cs
+++ src/Presentation/OrchestrationLab.Api/Controllers/WeatherForecastController.cs
@@ -23,6 +23,22 @@ namespace OrchestrationLab.Api.Controllers
         {
             _logger.LogInformation("Getting weather forecast");
 
+            var random = new Random();
+            var list = new List<byte[]>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                var data = new byte[102400];
+                random.NextBytes(data);
+
+                list.Add(data); // Change the size here.
+
+                if (i % 2 == 0)
+                    list.RemoveAt(0);
+
+                Thread.Sleep(100); // Change the wait time here.
+            }
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
